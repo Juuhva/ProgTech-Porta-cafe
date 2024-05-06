8b46e6e33d14dc45ced1428795600929bfeac876
Pántya Barnabás <pantya.barnabas@gmail.com>
2024-05-06 20:58:18 +0200

    Üdvözlő képernyő elkészítése

---
+++ PortaCafe/src/CafeApp/CafeMainForm.java
@@ -0,0 +1,12 @@
+package CafeApp;
+
+import javax.swing.*;
+
+public class CafeMainForm extends JFrame {
+
+    public CafeMainForm(){
+        setSize(1280, 720);
+        setResizable(false);
+        setVisible(true);
+    }
+}
--- PortaCafe/src/Main.java
+++ PortaCafe/src/Main.java
@@ -1,5 +1,13 @@
+import CafeApp.CafeWelcomeForm;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new CafeWelcomeForm();
+            }
+        });
     }
 }
