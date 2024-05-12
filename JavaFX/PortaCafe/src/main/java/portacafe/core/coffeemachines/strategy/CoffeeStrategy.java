f406d963a42c3a8fe8e439d40b1e13d1ea59e95e
Gabriel Balazs <jobaba2003@gmail.com>
2024-05-12 21:15:27 +0200

    CoffeeMachine update + Kiosk

--- JavaFX/PortaCafe/src/main/java/portacafe/core/coffeemachines/strategy/CoffeeStrategy.java
+++
@@ -1,9 +0,0 @@
-package portacafe.core.coffeemachines.strategy;
-
-import portacafe.core.coffees.Coffee;
-import portacafe.core.coffees.abstracts.AbstractCoffee;
-
-public interface CoffeeStrategy {
-
-    public boolean canMakeCoffee(Class<? extends Coffee> coffeeClass);
-}
